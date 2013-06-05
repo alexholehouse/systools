8dc1e6a639696db6b468fb0e3df9ddcd8201cab8
Alex <alex.holehouse@gmail.com>
2013-06-04 22:46:37 -0500

    added mounted bash script

---
+++ sysadmin/mounted.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# function to see if a directory is mounted from a non                           
+# local location (i.e. over a network)                                           
+function mounted() {                                                             
+                  
+    
+    # get current directory
+    dir=$(pwd)
+                                                               
+    # recursivly cycle through the directory name                                
+    # components checking to see if the full path                                
+    # is associated with a line in the /proc/mounts                              
+    # file which also contains an ip address     
+    while [ "$dir" != "/" ]
+    do                                                                           
+        if grep "[0-255]\.[0-255]" /proc/mounts | grep -qs "$dir""\+ " ;then     
+            echo "MOUNTED DIRECTORY"                                             
+            return                                                               
+        fi                                                                               
+        dir=$(dirname $dir)                                                      
+    done                                                                        
+    echo "LOCAL DIRECTORY"                                                       
+}  
